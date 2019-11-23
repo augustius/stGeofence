4d0f609837aa43cc3507cd7b7dac54db171700ae
augustius c <cokroeaugustius@gmail.com>
2019-11-23 21:12:45 +0800

    - add mapKit for visualization - add plist description for location - add button to show user current location - add button to add more geofence

--- stGeofence/ViewController.swift
+++ stGeofence/ViewController.swift
@@ -5,16 +5,44 @@
 //  Created by Augustius on 23/11/2019.
 //  Copyright © 2019 august. All rights reserved.
 //
+//
 
 import UIKit
+import MapKit
+import CoreLocation
+import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    @IBOutlet private weak var mapView: MKMapView!
+
+    private var locationManager : CLLocationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.mapView.delegate = self
+        self.locationManager.delegate = self
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.startUpdatingLocation()
     }
 
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        print("location updating......")
+    }
 
+    @IBAction private func currentLocationDidTap() {
+        mapView.zoomToUserLocation()
+    }
+
+    @IBAction private func addGeofence() {
+
+    }
 }
 
+extension MKMapView {
+  func zoomToUserLocation() {
+    guard let coordinate = userLocation.location?.coordinate else { return }
+    let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
+    setRegion(region, animated: true)
+  }
+}
